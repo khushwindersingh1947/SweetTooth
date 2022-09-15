aa154fb4bb00ae898eaef7c0c3b3b3286657cc25
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-09-15 17:04:04 -0400

    added the categories model class and updated the categories controller class

---
+++ SweetTooth/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SweetTooth.Models
+{
+    public class Category
+    {
+        //all primary keys should be called Id or class name followed by Id (CategoryId)
+        [Required]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+ 
+    }
+}
